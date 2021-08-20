8c0a32282201ff4c13ec406e4b5dbfe014b17341
superstark01 <superstark02@gmail.com>
2021-08-20 13:10:26 +0530

    123

--- assets/js/getProducts.js
+++ assets/js/getProducts.js
@@ -6,32 +6,46 @@ db.collection("Products").get().then((result) => {
     result.forEach(doc => {
         data.push(doc.data())
     });
-
+    console.log(data)
     document.getElementById("product").innerHTML = data.map(item => {
         return (
-            `<div class="col-lg-3 col-md-4 col-sm-6" >
-                <div class="single-grid-product mb-40" >
-                    <div class="product-image">
-                        <a href="product.html">
-                            <img src=${item.image}
-                                class="img-fluid" alt="" />
-                            <img src=${item.image1}
-                                class="img-fluid" alt="" />
-                        </a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"> <a href="product.html">${item.name}</a></h3>
-                        <p class="product-price"><span class="discounted-price">${item.sp}</span> <span
-                            class="main-price discounted">${item.mrp}</span></p>
-                    </div>
-                    <div class="product-price">
-                        <span class="discounted-price" >${item.discount}</span>
-                    </div>
-                </div>
-            </div>`
+            render(item)
         )
     })
 
 }).catch((err) => {
     console.log(err)
 });
+
+function render(item) {
+    return (
+        <div class="col-lg-3 col-sm-6 mb--45">
+            <div class="ft-product HTfadeInUp">
+                <div class="product-inner" style="display:flex; justify-content: space-between; flex-direction:column; height:400px;" >
+                    <div class="product-image" style="display:flex; justify-content:center; align-items:center; height:100%" >
+                        <figure class="product-image--holder">
+                            <img src="${item.image}"
+                                alt="Product" />
+                        </figure>
+                        <a href="product-details.html" class="product-overlay"></a>
+                    </div>
+                    <div class="product-info">
+                        <div class="product-category">
+                            <a href="product-details.html">${item.category}</a>
+                        </div>
+                        <h3 class="product-title"><a href="product-details.html">${item.name}</a></h3>
+                        <div class="product-info-bottom">
+                            <div class="product-price-wrapper">
+                                <span class="money">$${item.sp}</span>
+                            </div>
+                            <a href="cart.html" class="add-to-cart pr--15">
+                                <i class="fas fa-plus"></i>
+                                <span>Buy Now</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- assets/js/getUser.js
+++ assets/js/getUser.js
@@ -1,14 +1,35 @@
 
+
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        console.log("true")
-        document.getElementById("user").innerHTML = `<div><img src="${user.photoURL}" /></div>`
+        document.getElementById("login-button").hidden = true
+        document.getElementById("user-name").innerHTML = "Hi "+user.displayName
     } else {
-        document.getElementById("user").innerHTML = `<div><button class="login-btn" id="login-button" >Login</button></div>`
-        console.log("false")
+        document.getElementById("login-button").hidden = false
+        document.getElementById("login-image").hidden = true
     }
 });
 
-document.getElementsByClassName("login-auth").onclick(e=>{
-    console.log("Click");
-})
+document.getElementById("login-button").onclick = async function login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    try {
+        const result = await firebase.auth()
+            .signInWithRedirect(provider);
+
+            var temp = null
+
+            if(result.user.phoneNumber){
+                temp = result.user.phoneNumber
+            }
+
+        addDoc("Users", result.user.uid, {
+            name: result.user.displayName,
+            email: result.user.email,
+            phone: temp,
+            photo: result.user.photoURL,
+            uid: result.user.uid
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+}
